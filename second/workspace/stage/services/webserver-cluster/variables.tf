afe115a4c34c64bf9bb15bc7c177c53c0bfc6c36
treefrog18 <keafz0829@gmail.com>
2023-03-15 22:52:33 +0900

    terraform_remote_state

---
+++ second/workspace/stage/services/webserver-cluster/variables.tf
@@ -0,0 +1,16 @@
+# variable "db_remote_state_bucket" {
+#   description = "The name of the S3 bucket used for the database's remote state storage"
+#   type        = string
+# }
+
+# variable "db_remote_state_key" {
+#   description = "The name of the key in the S3 bucket used for the database's remote state storage"
+#   type        = string
+# }
+
+variable "server_port" {
+    description = "The port the server will use for HTTP request"
+    type        = number
+    default = 8080
+  
+}
